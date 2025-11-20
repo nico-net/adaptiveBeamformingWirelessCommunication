cd6255f8e9bf41bf3ab85d11ef8c027bf8327d22
Nicola Gallucci <nicolaprogammazione@gmail.com>
2025-11-20 14:50:06 +0100

    changes

--- main.m
+++ main.m
@@ -2,7 +2,8 @@ close all;
 clear all;
 clc;
 
-Pars.fc = 1e9; % Carrier frequency
+%% Parameters
+Pars.fc = 1e9; 
 Pars.c = physconst('LightSpeed');
 Pars.lambda = Pars.c / Pars.fc;
 Pars.Fsin = 600;
@@ -13,41 +14,56 @@ Pars.TsVect = 0:Pars.Ts:5/Pars.Fsin;
 waveform1 = sin(2*pi*Pars.Fsin*Pars.TsVect);
 waveform2 = waveform1;
 
-% Geometry
-Geometry.BSPos = [0,0,25]; % 25m of height
-Geometry.V1PosStart = [70, -100, 1.5];
-Geometry.V1PosEnd = [70, 100, 1.5];
-Geometry.V2PosStart = [200, -50, 1.5];
-Geometry.V2PosEnd = [10, -50, 1.5];
+%% 2D Geometry (x,y only)
+Geometry.BSPos     = [0, 0];  % BS at origin, 2D
 
-% Distances
-Geometry.T1 = sqrt(sum((Geometry.V1PosEnd(1,1:2) - Geometry.V1PosStart(1,1:2)).^2));
-Geometry.DistV1Start =sqrt(sum((Geometry.V1PosStart(1,1:2) - Geometry.BSPos(1,1:2)).^2));
-Geometry.T2 = sqrt(sum((Geometry.V2PosEnd(1,1:2) - Geometry.V2PosStart(1,1:2)).^2));
-Geometry.DistV2Start =sqrt(sum((Geometry.V2PosStart(1,1:2) - Geometry.BSPos(1,1:2)).^2));
+Geometry.V1PosStart = [70, -100];
+Geometry.V1PosEnd   = [70,  100];
+
+Geometry.V2PosStart = [200, -50];
+Geometry.V2PosEnd   = [10,  -50];
+
+%% Distances (2D)
+Geometry.T1 = norm(Geometry.V1PosEnd - Geometry.V1PosStart);
+Geometry.DistV1Start = norm(Geometry.V1PosStart - Geometry.BSPos);
+
+Geometry.T2 = norm(Geometry.V2PosEnd - Geometry.V2PosStart);
+Geometry.DistV2Start = norm(Geometry.V2PosStart - Geometry.BSPos);
+
+%% **Only Azimuth Angles in 2D**
+Geometry.AOAV1Start = atan2(Geometry.BSPos(2) - Geometry.V1PosStart(2), ...
+                            Geometry.BSPos(1) - Geometry.V1PosStart(1));
+
+Geometry.AOAV2Start = atan2(Geometry.BSPos(2) - Geometry.V2PosStart(2), ...
+                            Geometry.BSPos(1) - Geometry.V2PosStart(1));
+
+% DOA vectors for phased array (azimuth-only)
+Geometry.DOAV1S = [Geometry.AOAV1Start; 0];  % Zero elevation
+Geometry.DOAV2S = [Geometry.AOAV2Start; 0];
+
+%% FSPL
+% Calculate the Free Space Path Loss (FSPL) 
+Geometry.FSPLv1 = (4*pi*Geometry.DistV1Start/Pars.lambda)^2;
+Geometry.FSPLv2 = (4*pi*Geometry.DistV2Start/Pars.lambda)^2;
+
+waveform1 = waveform1 / sqrt(Geometry.FSPLv1);
+waveform2 = waveform2 / sqrt(Geometry.FSPLv1);
+
+%% BS Array
+Geometry.BSarray = phased.ULA('NumElements', 4, ...
+    'ElementSpacing', Pars.lambda/2);
+
+figure;
+viewArray(Geometry.BSarray ,'Title','ULA 2D View')
+set(gca,'CameraViewAngle',4.4);
+
+%% Received signal
+recivedW = collectPlaneWave(Geometry.BSarray, ...
+    [waveform1', waveform2'], ...         % signals
+    [Geometry.DOAV1S Geometry.DOAV2S], ... % [az; el]
+    Pars.fc);
+
+Pars.SNR = 10;
+chOut = awgn(recivedW, Pars.SNR, 'measured');
 
 
-
-Geometry.AOAV1Start = atan2(Geometry.BSPos(1,2) - Geometry.V1PosStart(1,2), ...
-    Geometry.BSPos(1,1) - Geometry.V1PosStart(1,1)*180/pi);
-Geometry.ZOAV1Start = atan2(Geometry.DistV1Start, ...
-    Geometry.BSPos(1,3) - Geometry.V1PosStart(1,3)*180/pi);
-Geometry.DOAV1S = [Geometry.AOAV1Start Geometry.ZOAV1Start];
-Geometry.AOAV2Start = atan2(Geometry.BSPos(1,2) - Geometry.V2PosStart(1,2), ...
-    Geometry.BSPos(1,1) - Geometry.V2PosStart(1,1)*180/pi);
-Geometry.ZOAV2Start = atan2(Geometry.DistV2Start, ...
-    Geometry.BSPos(1,3) - Geometry.V2PosStart(1,3)*180/pi);
-Geometry.DOAV2S = [Geometry.AOAV2Start Geometry.ZOAV2Start];
-
-
-Geometry.BSarray = phased.URA('Size',[4 4], ...
-    'ElementSpacing',[Pars.lambda/2 Pars.lambda/2], ...
-    'ArrayNormal','x');
-Geometry.BSAntennaPos = getElementPosition(Geometry.BSarray);
-
-recivedW = collectPlaneWave(Geometry.BSarray,[waveform1 waveform2], ...
-    [Geometry.DOAV1S' Geometry.DOAV2S'], Pars.fc);
-
-Pars.SNR = 20;
-
-chOut = awgn(recivedW, Pars.SNR,'measured');
